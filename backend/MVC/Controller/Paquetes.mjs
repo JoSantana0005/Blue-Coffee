d17d9f55551e886c6914f5a6179d312a3d5eda29
JoSantana0005 <joseasantana05@gmail.com>
2025-03-01 16:20:35 -0400

    Haciendo los controladores de los paquetes

--- backend/MVC/Controller/Paquetes.mjs
+++ backend/MVC/Controller/Paquetes.mjs
@@ -2,4 +2,27 @@ export class ProductController{
     constructor({ProductModels}){
         this.ProductModels = ProductModels;
     }
+
+    // Obtener todos los paquetes de cafe
+    getAll = async ( req, res) => {
+        try{
+            const paquetes = await this.ProductModels.getALL();
+            return res.status(200).json(paquetes);
+        }
+        catch(err){
+            console.log(`Ocurrio un error ${err}`);
+            return res.status(500).json({error: err});
+        }
+    }
+    getByPrice = async ( req, res) => {
+        try{
+            const {price} = req.params;
+            const paquetePrice = await this.ProductModels.getByPrice({price});
+            return res.status(200).json(paquetePrice);
+        }
+        catch(err){
+            console.log(`Ocurrio un error ${err}`);
+            return res.status(500).json({error: err});
+        }
+    }
 }
