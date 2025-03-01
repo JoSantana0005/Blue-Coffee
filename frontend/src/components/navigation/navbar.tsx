26fc13ae786e8ca28e8b1b71dcc050f04b0dbd7b
cesardarizaleta <cesardarizaleta@gmail.com>
2025-03-01 16:38:19 -0400

    Agregando Animacion

--- frontend/src/components/navigation/navbar.tsx
+++ frontend/src/components/navigation/navbar.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import { Link } from "react-router";
 import './styles/navbar.css';
 import {User, ShoppingBasket} from "lucide-react";
 
 export default function Navbar() {
+
+    const cart = useRef(null)
+
+    function handleCart(){
+        //cart.current.style.right = 0
+    }
+
     return (
         <nav className="fixed top-0 bg-white w-full h-16 flex p-8 px-14 items-center justify-between">
             <Link to="/"><img src="/assets/brand/BlueCoffee.svg" alt="Logo" /></Link>
@@ -14,9 +21,12 @@ export default function Navbar() {
                 <Link to="/contact">Packs</Link>
                 <Link to="/contact">Accesorios</Link>
             </section>
-            <section id="accountSection" className="flex items-center gap-6">
+            <section onClick={handleCart} id="accountSection" className="flex items-center gap-6">
                 <Link to="/login"><User size={40} className="hover:bg-primary p-2 hover:rounded-full transition-all" /></Link>
-                <span><ShoppingBasket size={40} className="hover:bg-primary p-2 hover:rounded-full transition-all cursor-pointer" /></span>
+                <span><ShoppingBasket size={40} className="hover:bg-primary active:scale-95 p-2 hover:rounded-full transition-all cursor-pointer" /></span>
+            </section>
+            <section ref={cart} className="fixed group-active:right-0 transition-all right-full top-0 w-1/4 h-full bg-white">
+
             </section>
         </nav>
     );
--- frontend/src/components/sections/hero.tsx
+++ frontend/src/components/sections/hero.tsx
@@ -3,9 +3,12 @@ import './styles/hero.css'
 
 export default function Hero(){
     return (
-        <section id="hero" className='grid grid-cols-3 grid-rows-3 gap-4 w-full h-screen *:rounded-xl'>
-            <article className="col-span-2 row-span-2 bg-gray-200 overflow-hidden">1</article>
-            <article className='col-start-3 bg-gray-200'>2</article>
+        <section id="hero" className='grid max-md:grid-cols-1 max-md:grid-rows-4 grid-cols-3 grid-rows-3 gap-6 w-full p-6 h-screen *:rounded-xl'>
+            <article className="col-span-2 row-span-2 overflow-hidden"></article>
+            <article className='col-start-3 bg-primary flex items-center justify-center gap-2'>
+                <img className="size-28" src="/assets/brand/Apasionados.svg" alt="Apasionados" />
+                <h2 className="text-white font-bold text-3xl uppercase">Apasionados<br></br> por el Cafe</h2>
+            </article>
             <article className="row-span-2 col-start-3 row-start-2 bg-gray-200">3</article>
             <article className="col-span-2 row-start-3 bg-gray-200">4</article>
         </section>
