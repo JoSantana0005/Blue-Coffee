52e3e2cbd6073ba7912196e9680df10a5034ffac
JoSantana0005 <joseasantana05@gmail.com>
2025-03-01 20:07:37 -0400

    Validaciones de login y register colocadas los demas lo hace cesar

--- backend/Js/auth/Login.js
+++ backend/Js/auth/Login.js
@@ -1,4 +1,5 @@
 // Function para validar el correo
+
 function validateEmail(email){
     if(email){
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -11,6 +12,28 @@ function validateEmail(email){
 }
 
 // Function para validar la contraseña
+
 function validatePassword(password){
-    
+    if(password && password.length >= 8){
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        return regex.test(password);
+    }
+    else{
+        console.log("No existen el campo de contraseña o la contraseña no es valida");
+        return false;
+    }
+}
+
+// Aqui una validacion de que todos los campos este completos
+
+function validateComplete(email,password){
+    if(email && password){
+        return 'Todos los campos esta completos';
+    }
+    else if(email || password){
+        return 'No todos los campos estan completos';
+    }
+    else{
+        return 'Ningun campo esta completo';
+    }
 }
