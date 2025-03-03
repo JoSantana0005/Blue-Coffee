1f08103fbd6ab6da462e2260fcdf3c4f407236bc
cesardarizaleta <cesardarizaleta@gmail.com>
2025-03-03 12:11:37 -0400

    Arreglando Imagenes

--- frontend/src/components/ui/CapsulaCard.tsx
+++ frontend/src/components/ui/CapsulaCard.tsx
@@ -6,7 +6,7 @@ import AddCart from "./AddCart";
 export default function CapsulaCard({...item}: Capsula){
     return (
         <article className="h-72 cursor-pointer border-[#00000030] hover:border w-full transition-all rounded-lg p-2 flex flex-col justify-between items-center">
-            <img src={item.imagen_capsula} alt={item.name_capsula} className="rounded-lg h-2/3"/>
+            <img src={"/data/images/capsulas/capsula.webp"} alt={item.name_capsula} className="rounded-lg h-2/3"/>
             <div className="flex flex-col w-full justify-between items-center h-1/3">
                 <h3 className="text-secondary font-black text-sm text-center">{item.name_capsula}</h3>
                 <p className="text-secondary font-light text-xs text-center">${item.price_capsula}</p>
